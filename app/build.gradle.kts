8e70563cea01fa79bdb1435dd7623c645c2d8bab
Waister Nunes <waisters@gmail.com>
2026-08-08 11:57:23 -0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -124,6 +124,5 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 }
